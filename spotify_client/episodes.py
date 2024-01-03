feb643df5df2413b60c91866fc370b9605b2f1bd
krisgholson <kris@idea2.dev>
2024-01-03 10:28:34 -0500

    data

--- spotify_client/episodes.py
+++ spotify_client/episodes.py
@@ -1,5 +1,6 @@
 import os
 import httpx
+import time
 from dotenv import load_dotenv
 
 load_dotenv() 
@@ -8,14 +9,25 @@ SPOTIFY_API_URL = os.getenv("SPOTIFY_API_URL")
 SPOTIFY_ACCESS_TOKEN = os.getenv("SPOTIFY_ACCESS_TOKEN")
 SPOTIFY_HEADERS = {"authorization": f"Bearer {SPOTIFY_ACCESS_TOKEN}"}
 
-def get_episodes(show_id):
-    print(f"get_episodes: {show_id}")
-    r = httpx.get(f"{SPOTIFY_API_URL}/shows/{show_id}/episodes?limit=20&offset=0", headers=SPOTIFY_HEADERS)
-    print(r.status_code)
-    print(r.text)
+def get_all(url, items=[]):
+    r = httpx.get(url, headers=SPOTIFY_HEADERS)
+    resp = r.json()
+    next_url = resp["next"]
+    resp_items = resp.get("items", [])
+    items = items + resp_items
+    if next_url:
+        time.sleep(.300)
+        return get_all(next_url, items)
+    else:
+        return items
+
+def get_all_episodes(show_id):
+    url = f"{SPOTIFY_API_URL}/shows/{show_id}/episodes?limit=20&offset=0"
+    episodes = get_all(url)
+    print(len(episodes))
 
 def main():
-    get_episodes("753FVUsio4Y6GjFvbGpvF0")
+    get_all_episodes("753FVUsio4Y6GjFvbGpvF0")
 
 if __name__ == '__main__':
     main()
